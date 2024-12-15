830f396077d457f87d6a53c910be584c1c04834a
ysaaba <nickschack@gmail.com>
2024-12-15 09:17:10 +0100

    Update Vite base path to '/Chordie-App/' and add .nojekyll file creation in deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Chordie-App/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
