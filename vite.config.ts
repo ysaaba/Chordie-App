26d72edabf4713807b26c1722d9b0eaededffcec
ysaaba <nickschack@gmail.com>
2025-05-06 00:57:49 +0200

    fix: Ensure base path configuration is set correctly in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Chordie-App/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
